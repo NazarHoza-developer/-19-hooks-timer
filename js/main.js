e39c5b259a61afbfed1f582fbb1827c8e1ed8091
nazarhoza <nazarhoza@gmail.com>
2022-11-01 08:43:02 +0200

    new

---
+++ js/main.js
@@ -0,0 +1,96 @@
+const hourElement = document.querySelector('.hour')
+const minuteElement = document.querySelector('.minute')
+const secondElement = document.querySelector('.second')
+const millisecondElement = document.querySelector('.millisecond')
+
+const startButton = document.querySelector('.start')
+const pauseButton = document.querySelector('.pause')
+const stopButton = document.querySelector('.stop')
+const newButton = document.querySelector('.new')
+
+startButton.addEventListener('click', ()=> {
+    clearInterval(interval)
+    interval = setInterval(startTimer, 10)
+})
+
+pauseButton.addEventListener('click', ()=>{
+    clearInterval(interval)
+    interval = setInterval(startTimer, 10)
+})
+
+
+stopButton.addEventListener('click', ()=>{
+    clearInterval(interval)
+    clearFields()   
+} )
+
+
+newButton.addEventListener('click', () => {
+    clearInterval(interval)
+    counter++
+    const results = document.querySelector('.results')
+    const block = document.createElement('div')
+    block.classList.add('results__info')
+    block.innerText = `Result: ${hour}:${minute}:${second}:${millisecond}`
+    results.append(block)
+    clearFields()
+    clearInterval(interval)
+    interval = setInterval(startTimer, 10)
+
+
+})
+
+
+let hour = 00,
+    minute = 00,
+    second = 00,
+    millisecond = 00,
+    interval
+    counter = 0
+function startTimer() {
+    millisecond++
+    if(millisecond < 9) {
+        millisecondElement.innerText = "0" + millisecond
+    }
+    if(millisecond > 9) {
+        millisecondElement.innerText = millisecond
+    }
+    if(millisecond > 99) {
+        second++
+        secondElement.innerText = "0" + second
+        millisecond = 0
+        millisecondElement.innerText = "0" + millisecond
+    }
+    if(second) {
+        secondElement.innerText = "0" + second
+    }
+    if(second > 9 ) {
+        secondElement.innerText = second
+    }
+    if(second > 59){
+        minute++
+        minuteElement.innerText = "0" - minute
+        second = 0
+        secondElement.innerText = "0" - second
+    }
+
+
+    if(minute > 9){
+        minuteElement.innerText = minute
+    }
+
+
+    if(hour > 9){
+        minuteElement.innerText = hour
+    }
+}
+function clearFields(){
+    hour = 00
+    minute = 00
+    second = 00
+    millisecond = 00
+    hourElement.textContent = "00"
+    minuteElement.textContent = "00"
+    secondElement.textContent = "00"
+    millisecondElement.textContent = "00"
+}
